6ddc84b6a55e5c019d2b982093de56a2a34c3176
MinervasOwl <MinervasOwl@users.noreply.github.com>
2022-02-20 14:39:43 -0500

    Write function to play multiple rounds

--- wordle_solver/test_players.py
+++ wordle_solver/test_players.py
@@ -7,15 +7,20 @@ from lib.player import RandomPlayer
 RANDOM_STATE_GAME = 123
 RANDOM_STATE_PLAYER = 1234
 
-bag_words = pd.read_csv(r"wordle_solver\datasets\5letter_words_top5000.csv")
+bag_words_top5000 = pd.read_csv(r"wordle_solver\datasets\5letter_words_top5000.csv")
 
 game = Game(RANDOM_STATE_GAME)
-game.set_bag_words(bag_words)
 random_player = RandomPlayer(RANDOM_STATE_PLAYER)
-random_player.set_bag_words(bag_words)
 
-game_won_count = 0
-for _ in range(100):
-    game_won_count = game_won_count + game.play(random_player)
+def play_multiple_rounds(game, player, bag_words, num_rounds):
+    game.set_bag_words(bag_words)
+    player.set_bag_words(bag_words)
+    game_won_count = 0
+    for _ in range(num_rounds):
+        game_won_count = game_won_count + game.play(player)
+    return game_won_count
+
+game_won_count = play_multiple_rounds(
+    game, random_player, bag_words_top5000, 100)
 
 print(game_won_count)
