22361b570eea1c38486fa24483b1e52363cf2439
MinervasOwl <MinervasOwl@users.noreply.github.com>
2022-02-20 14:18:39 -0500

    Rewrite bag word creation, no impact

--- wordle_solver/misc/create_bag_words.py
+++ wordle_solver/misc/create_bag_words.py
@@ -1,18 +1,18 @@
 """Create a bag of 5-letter words used by Wordle game."""
 import pandas as pd
 
-df = pd.read_csv(r"wordle_solver\datasets\unigram_freq.csv")
 
-df_5letter = df[df["word"].str.len() == 5]
+def save_words_with_likelihood(df, destination):
+    df["likelihood"] = df["count"].div(df["count"].sum())
+    df[["word", "likelihood"]].to_csv(destination, index=False)
 
-df_5letter["likelihood"] = df_5letter["count"] / df_5letter["count"].sum()
 
-df_5letter[["word", "likelihood"]].to_csv(
-    r"wordle_solver\datasets\5letter_words.csv", index=False)
+df_words = pd.read_csv(r"wordle_solver\datasets\unigram_freq.csv")
 
-df_5letter_top5000 = df_5letter.head(5000)
-df_5letter_top5000["likelihood"] = (
-    df_5letter_top5000["count"] / df_5letter_top5000["count"].sum())
+df_5letter = df_words[df_words["word"].str.len() == 5].copy(deep=True)
+save_words_with_likelihood(
+     df_5letter, r"wordle_solver\datasets\5letter_words.csv")
 
-df_5letter_top5000[["word", "likelihood"]].to_csv(
-    r"wordle_solver\datasets\5letter_words_top5000.csv", index=False)
+df_5letter_top5000 = df_5letter.head(5000).copy(deep=True)
+save_words_with_likelihood(
+    df_5letter_top5000, r"wordle_solver\datasets\5letter_words_top5000.csv")
