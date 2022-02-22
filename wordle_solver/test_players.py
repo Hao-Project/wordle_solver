18c3fd0084ce8d9b2461da95ca966c328a2c050e
MinervasOwl <MinervasOwl@users.noreply.github.com>
2022-02-21 19:28:31 -0500

    Summarize random strategy win rates
    Save in the summary file the win rates when the bag of words is using
    top-100/1000/5000 or all 5-letter words.

--- wordle_solver/test_players.py
+++ wordle_solver/test_players.py
@@ -4,13 +4,19 @@ import pandas as pd
 from lib.game import Game
 from lib.player import RandomPlayer
 
+DATA_PATH = r"wordle_solver\datasets"
+SUMMARY_PATH = r"wordle_solver\summary"
+
 RANDOM_STATE_GAME = 123
 RANDOM_STATE_PLAYER = 1234
 
-bag_words_top5000 = pd.read_csv(r"wordle_solver\datasets\5letter_words_top5000.csv")
-
-game = Game(RANDOM_STATE_GAME)
-random_player = RandomPlayer(RANDOM_STATE_PLAYER)
+LABEL_TO_DATA = {
+    "Top 100 Words": r"\5letter_words_top100.csv",
+    "Top 1000 Words": r"\5letter_words_top1000.csv",
+    "Top 5000 Words": r"\5letter_words_top5000.csv",
+    "All Words": r"\5letter_words.csv",
+}
+TESTING_ROUNDS = [10, 100, 1000, 10000]
 
 def play_multiple_rounds(game, player, bag_words, num_rounds):
     game.set_bag_words(bag_words)
@@ -20,7 +26,27 @@ def play_multiple_rounds(game, player, bag_words, num_rounds):
         game_won_count = game_won_count + game.play(player)
     return game_won_count
 
-game_won_count = play_multiple_rounds(
-    game, random_player, bag_words_top5000, 100)
+def main():
+    word_bags = {}
+    for key, data in LABEL_TO_DATA.items():
+        word_bags[key] = pd.read_csv(DATA_PATH + data)
 
-print(game_won_count)
+    game = Game(RANDOM_STATE_GAME)
+    random_player = RandomPlayer(RANDOM_STATE_PLAYER)
+
+    win_rates = {}
+    for key, bag_words in word_bags.items():
+        win_rates[key] = []
+        for num_rounds in TESTING_ROUNDS:
+            win_rate = play_multiple_rounds(
+                game, random_player, bag_words, num_rounds) / num_rounds
+            win_rates[key].append(win_rate)
+
+    df_win_rates = (
+        pd.DataFrame(win_rates, TESTING_ROUNDS)
+        .rename_axis(index="Num of Rounds", columns="Win Rates"))
+    print(df_win_rates)
+    df_win_rates.to_csv(SUMMARY_PATH + r"\random_strategy_win_rates.csv")
+
+if __name__ == "__main__":
+    main()
